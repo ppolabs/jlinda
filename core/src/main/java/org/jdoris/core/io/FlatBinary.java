ba0aaa18191d8402a93231405989623e3c079ca2
Petar Marinkovic <petarmar@gmail.com>
2011-04-14 20:48:38 +0200

    add FlatBinary class and junit tests

---
+++ core/src/main/java/org/jdoris/core/io/FlatBinary.java
@@ -0,0 +1,201 @@
+package org.jdoris.core.io;
+
+import org.apache.log4j.Logger;
+
+import java.awt.*;
+import java.io.*;
+
+public class FlatBinary {
+
+    private static Logger logger = Logger.getLogger(FlatBinary.class.getName());
+
+    File file;
+    String format;
+    long sizeBytes;
+    Rectangle dimensions;
+    int multiLookX;
+    int multiLookY;
+    DataOutputStream outStream;
+    DataInputStream inStream;
+
+    public double[][] data;
+
+    public FlatBinary() {
+
+    }
+
+    public boolean checkExists() {
+        return file.exists();
+    }
+
+    public boolean checkCanRead() throws FileNotFoundException {
+        return file.canRead();
+    }
+
+    public boolean checkCanWrite() throws FileNotFoundException {
+        return file.canWrite();
+    }
+
+    public void setInStream() throws FileNotFoundException {
+        inStream = new DataInputStream(new BufferedInputStream(new FileInputStream(file.getAbsoluteFile())));
+    }
+
+    public void setOutStream() throws FileNotFoundException {
+        outStream = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file.getAbsoluteFile())));
+    }
+
+    public void readDoubleFromStream() throws FileNotFoundException {
+        data = new double[dimensions.width][dimensions.height];
+        for (int i = 0; i < dimensions.width; i++) {
+            for (int j = 0; j < dimensions.height; j++) {
+                try {
+                    data[i][j] = inStream.readDouble();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public void writeDoubleToStream() throws FileNotFoundException {
+        for (int i = 0; i < dimensions.width; i++) {
+            for (int j = 0; j < dimensions.height; j++) {
+                try {
+                    outStream.writeDouble(data[i][j]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        try {
+            this.outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setData(double[][] data) {
+        this.data = data;
+    }
+
+    public void create() {
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void create(File genericFile) {
+        try {
+            genericFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void create(String genericFileName) {
+        this.create(new File(genericFileName));
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer();
+        sb.append("FlatBinary");
+        sb.append("{file=").append(file.getAbsoluteFile());
+        sb.append(", format='").append(format).append('\'');
+        sb.append(", sizeBytes=").append(sizeBytes);
+        sb.append(", dimensions=").append(dimensions);
+        sb.append(", multiLookX=").append(multiLookX);
+        sb.append(", multiLookY=").append(multiLookY);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FlatBinary that = (FlatBinary) o;
+
+        if (multiLookX != that.multiLookX) return false;
+        if (multiLookY != that.multiLookY) return false;
+        if (sizeBytes != that.sizeBytes) return false;
+        if (dimensions != null ? !dimensions.equals(that.dimensions) : that.dimensions != null) return false;
+        if (file != null ? !file.equals(that.file) : that.file != null) return false;
+        if (format != null ? !format.equals(that.format) : that.format != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = file != null ? file.hashCode() : 0;
+        result = 31 * result + (format != null ? format.hashCode() : 0);
+        result = 31 * result + (int) (sizeBytes ^ (sizeBytes >>> 32));
+        result = 31 * result + (dimensions != null ? dimensions.hashCode() : 0);
+        result = 31 * result + multiLookX;
+        result = 31 * result + multiLookY;
+        return result;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void setFileName(String genericFileName) {
+        this.file = new File(genericFileName);
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public void setSizeBytes(long sizeBytes) {
+        this.sizeBytes = sizeBytes;
+    }
+
+    public void setDimensions(Rectangle dimensions) {
+        this.dimensions = dimensions;
+    }
+
+    public void setMultiLookX(int multiLookX) {
+        this.multiLookX = multiLookX;
+    }
+
+    public void setMultiLookY(int multiLookY) {
+        this.multiLookY = multiLookY;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public long getSizeBytes() {
+        return sizeBytes;
+    }
+
+    public Rectangle getDimensions() {
+        return dimensions;
+    }
+
+    public int getMultiLookX() {
+        return multiLookX;
+    }
+
+    public int getMultiLookY() {
+        return multiLookY;
+    }
+
+
+
+
+}
---
+++ core/src/test/java/org/jdoris/core/io/FlatBinaryTest.java
@@ -0,0 +1,85 @@
+package org.jdoris.core.io;
+
+import org.junit.*;
+
+import java.awt.*;
+import java.io.File;
+
+public class FlatBinaryTest {
+
+    private static FlatBinary flatBinaryRead;
+    private static FlatBinary flatBinaryWrite;
+    private static Rectangle testDataRectangle;
+
+    private static double[][] testData;
+
+    @BeforeClass
+    public static void setupTestData() throws Exception {
+
+        testDataRectangle = new Rectangle(123, 321);
+        testData = new double[testDataRectangle.width][testDataRectangle.height];
+
+        for (int i = 0; i < testDataRectangle.width; i++) {
+            for (int j = 0; j < testDataRectangle.height; j++) {
+                testData[i][j] = Math.random();
+            }
+        }
+
+        flatBinaryRead = new FlatBinary();
+        flatBinaryWrite = new FlatBinary();
+
+        flatBinaryRead.setFile(new File("test.in"));
+        flatBinaryWrite.setFile(new File("test.out"));
+
+        flatBinaryWrite.setOutStream();
+
+    }
+
+    @AfterClass
+    public static void cleanTestData() {
+
+//        if (!testFile.exists())
+//            throw new IllegalArgumentException("Delete: no such file or directory: " + testFile.getName());
+//
+//        boolean success = testFile.delete();
+//
+//        if (!success)
+//            throw new IllegalArgumentException("Delete: deletion of file" + testFile.getName() + " failed");
+//
+//        System.gc();
+
+    }
+
+    @Test
+    public void testCreateAndCheck() throws Exception {
+        flatBinaryWrite.create();
+        Assert.assertEquals("File creating: ", true, flatBinaryWrite.checkExists());
+    }
+
+    // TODO: brake up this test in smaller test blocks
+    @Test
+    public void testWritingReadingData() throws Exception {
+
+        flatBinaryWrite.setDimensions(new Rectangle(testDataRectangle));
+        flatBinaryWrite.setData(testData);
+        flatBinaryWrite.writeDoubleToStream();
+
+        flatBinaryRead.setFile(flatBinaryWrite.file);
+        flatBinaryRead.setDimensions(new Rectangle(testDataRectangle));
+        flatBinaryRead.setInStream();
+        flatBinaryRead.readDoubleFromStream();
+
+        Assert.assertArrayEquals(flatBinaryRead.data,testData);
+
+    }
+
+    @Ignore
+    @Test
+    public void testCanRead() throws Exception {
+    }
+
+    @Ignore
+    @Test
+    public void testCanWrite() throws Exception {
+    }
+}
