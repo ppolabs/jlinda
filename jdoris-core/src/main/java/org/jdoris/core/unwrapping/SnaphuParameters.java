60ffac3d5e2cb03b44065cef5a55466bdac7ce2b
Petar Marinkovic <petarmar@gmail.com>
2012-03-02 11:02:48 +0100

    SNAPHU support: base classes for constructing snaphu.conf files

---
+++ jdoris-core/src/main/java/org/jdoris/core/unwrapping/SnaphuParameters.java
@@ -0,0 +1,91 @@
+package org.jdoris.core.unwrapping;
+
+public class SnaphuParameters {
+
+    public String outFileName;   // output filename
+//    public String outFileFormat; // output format [hgt] real4
+    public String unwrapMode;    // snaphu TOPO DEFO SMOOTH NOSTATCOSTS
+    public String logFileName;   // log filename for snaphu
+    public String coherenceFileName; // coherence filename for snaphu opt
+    public String verbosityFlag;     // snaphu TRUE or FALSE
+    public String snaphuInit;        // snaphu MST or MCF
+
+/*
+    // for parallel processing
+    public int nTileRow;
+    public int nTileCol;
+    public int rowOverlap;
+    public int columnOverlap;
+    public int numProcessors;
+    public int tileCostThreshold;
+*/
+
+    public SnaphuParameters() {
+    }
+
+    public SnaphuParameters(String outFileName, String unwrapMode,
+                            String logFileName, String coherenceFileName, String verbosityFlag,
+                            String snaphuInit) {
+        this.outFileName = outFileName;
+//        this.outFileFormat = outFileFormat;
+        this.unwrapMode = unwrapMode;
+        this.logFileName = logFileName;
+        this.coherenceFileName = coherenceFileName;
+        this.verbosityFlag = verbosityFlag;
+        this.snaphuInit = snaphuInit;
+    }
+
+    public void setOutFileName(String outFileName) {
+        this.outFileName = outFileName;
+    }
+
+//    public void setOutFileFormat(String outFileFormat) {
+//        this.outFileFormat = outFileFormat;
+//    }
+
+    public void setUnwrapMode(String unwrapMode) {
+        this.unwrapMode = unwrapMode;
+    }
+
+    public void setLogFileName(String logFileName) {
+        this.logFileName = logFileName;
+    }
+
+    public void setCoherenceFileName(String coherenceFileName) {
+        this.coherenceFileName = coherenceFileName;
+    }
+
+    public void setVerbosityFlag(String verbosityFlag) {
+        this.verbosityFlag = verbosityFlag;
+    }
+
+    public void setSnaphuInit(String snaphuInit) {
+        this.snaphuInit = snaphuInit;
+    }
+/*
+
+    public void setnTileRow(int nTileRow) {
+        this.nTileRow = nTileRow;
+    }
+
+    public void setnTileCol(int nTileCol) {
+        this.nTileCol = nTileCol;
+    }
+
+    public void setRowOverlap(int rowOverlap) {
+        this.rowOverlap = rowOverlap;
+    }
+
+    public void setColumnOverlap(int columnOverlap) {
+        this.columnOverlap = columnOverlap;
+    }
+
+    public void setNumProcessors(int numProcessors) {
+        this.numProcessors = numProcessors;
+    }
+
+    public void setTileCostThreshold(int tileCostThreshold) {
+        this.tileCostThreshold = tileCostThreshold;
+    }
+*/
+}
