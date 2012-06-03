bf90e838236b9324da72c62a42c69afb0c0b9678
Petar Marinkovic <petarmar@gmail.com>
2012-06-03 18:11:44 +0200

    DInSAR core class together with its unit tests
     - Implementation that works on the whole tile/buffer, no iterations over buffer lines

--- jdoris-core/src/main/java/org/jdoris/core/geocode/DInSAR.java
+++ jdoris-core/src/main/java/org/jdoris/core/geocode/DInSAR.java
@@ -1,22 +1,19 @@
 package org.jdoris.core.geocode;
 
+import org.jblas.ComplexDoubleMatrix;
 import org.jblas.DoubleMatrix;
-import org.jblas.Solve;
+import org.jblas.MatrixFunctions;
 import org.jdoris.core.Baseline;
-import org.jdoris.core.Ellipsoid;
 import org.jdoris.core.Orbit;
 import org.jdoris.core.SLCImage;
-import org.jdoris.core.todo_classes.todo_classes;
-import org.jdoris.core.utils.LinearAlgebraUtils;
-
-import static org.jdoris.core.utils.PolyUtils.normalize2;
+import org.jdoris.core.Window;
+import org.jdoris.core.utils.MathUtils;
+import org.jdoris.core.utils.PolyUtils;
 
 /**
  * DInSAR prototype class
  * <p/>
  * Differential insar with an unwrapped topo interferogram (hgt or real4 format) and a wrapped(!) defo interf.
- * if r4 then NaN==-999 is problem with unwrapping, else hgt
- * if ampl. =0 then problem flagged with unwrapping.
  * <p/>
  * The topography is removed from the deformation interferogram by the formula (prime ' denotes defo pair):
  * <p/>
@@ -42,185 +39,189 @@ import static org.jdoris.core.utils.PolyUtils.normalize2;
 
 public class DInSAR {
 
-    public static void dinsar(
-            todo_classes.inputgeneral input_general,
-            todo_classes.input_dinsar dinsarinput,
-            Ellipsoid ellips,
-            SLCImage master,
-            Orbit masterorbit,
-            SLCImage defoslave,
-            Orbit defoorbit,
-            todo_classes.productinfo defointerferogram
-    ) throws Exception {
+    final private SLCImage masterMeta;
+    final private SLCImage slaveDefoMeta;
+    final private SLCImage topoSlaveMeta;
+    final private Orbit masterOrbit;
+    final private Orbit slaveDefoOrbit;
+    final private Orbit slaveTopoOrbit;
 
-        SLCImage toposlave = null;                                 // info on slave image
-        todo_classes.productinfo topounwrappedinterf = null;       // interferogram
-        Orbit toposlaveorbit = null;                               // always fill
+    private volatile Window dataWindow;
+    private volatile Window tileWindow;
 
-        // ______ Check 4 pass if topomaster is specified (diff than m_res) ______
-        boolean FOURPASS = false;                 // assume 4pass
+    private volatile DoubleMatrix topoData;
+    private volatile ComplexDoubleMatrix defoData;
 
-        // ______ Normalization factors for polynomial ______
-        double minL = master.getCurrentWindow().linelo;
-        double maxL = master.getCurrentWindow().linehi;
-        double minP = master.getCurrentWindow().pixlo;
-        double maxP = master.getCurrentWindow().pixhi;
+    public DInSAR(SLCImage masterMeta, SLCImage slaveDefoMeta, SLCImage topoSlaveMeta, Orbit masterOrbit, Orbit slaveDefoOrbit, Orbit slaveTopoOrbit) {
+        this.masterMeta = masterMeta;
+        this.slaveDefoMeta = slaveDefoMeta;
+        this.topoSlaveMeta = topoSlaveMeta;
+        this.masterOrbit = masterOrbit;
+        this.slaveDefoOrbit = slaveDefoOrbit;
+        this.slaveTopoOrbit = slaveTopoOrbit;
 
-        // ====== Model perpendicular baseline for master and slave ======
-        // ______ compute B on grid every 500 lines, 100 pixels
-        // ______ in window for topo/defo ______
-        int numpointsL = 20;                                  // grid for modelling
-        int numpointsP = 10;                                  // grid for modelling
-        double dlines = (toposlave.getCurrentWindow().linehi - toposlave.getCurrentWindow().linelo) /
-                (numpointsL - 1);
-        double dpixels = (toposlave.getCurrentWindow().pixhi - toposlave.getCurrentWindow().pixlo) /
-                (numpointsP - 1);
+        dataWindow = masterMeta.getCurrentWindow();
+    }
 
-        double[] LINENUMBER = new double[numpointsL * numpointsP];
-        double[] PIXELNUMBER = new double[numpointsL * numpointsP];
+    public void setDataWindow(Window dataWindow) {
+        this.dataWindow = dataWindow;
+    }
 
-        int k = 0;
+    public void setTileWindow(Window tileWindow) {
+        this.tileWindow = tileWindow;
+    }
 
-        // define the grid
-        for (int i = 0; i < numpointsL; ++i) {
-            for (int j = 0; j < numpointsP; ++j) {
-                LINENUMBER[k] = topounwrappedinterf.win.linelo + i * dlines;     // line coordinate
-                PIXELNUMBER[k] = topounwrappedinterf.win.pixlo + j * dpixels;     // pixel coordinate
-                ++k;
-            }
+    public void setTopoData(DoubleMatrix topoData) {
+        this.topoData = topoData;
+    }
+
+    public void setDefoData(ComplexDoubleMatrix defoData) {
+        this.defoData = defoData;
+    }
+
+    public ComplexDoubleMatrix getDefoData() {
+        return defoData;
+    }
+
+    public void dinsar() throws Exception {
+
+        // Normalization factors for polynomial
+        double minL = dataWindow.linelo;
+        double maxL = dataWindow.linehi;
+        double minP = dataWindow.pixlo;
+        double maxP = dataWindow.pixhi;
+
+        // Model perpendicular baseline for master and defo
+        // .....compute B on grid every 500 lines, 100 pixels
+        // .....in window for topoData/defoData
+        final int nPoints = 200;
+        final int[][] positionArray = MathUtils.distributePoints(nPoints, masterMeta.getCurrentWindow());
+        DoubleMatrix position = new DoubleMatrix(nPoints, 2);
+        for (int i = 0; i < nPoints; i++) {
+            position.put(i, 0, positionArray[i][0]);
+            position.put(i, 1, positionArray[i][1]);
         }
 
         // model baselines
-        Baseline topo_baseline = new Baseline();
-        topo_baseline.model(master, defoslave, masterorbit, defoorbit);
+        Baseline topoBaseline = new Baseline();
+        topoBaseline.model(masterMeta, topoSlaveMeta, masterOrbit, slaveTopoOrbit);
 
-        Baseline defo_baseline = new Baseline();
-        defo_baseline.model(master, toposlave, masterorbit, toposlaveorbit);
+        Baseline defoBaseline = new Baseline();
+        defoBaseline.model(masterMeta, slaveDefoMeta, masterOrbit, slaveDefoOrbit);
 
         double lastline = -1.0;
 
-        double[] Bperptopo = new double[LINENUMBER.length];
-        double[] Bperpdefo = new double[LINENUMBER.length];
+        DoubleMatrix bPerpTopo = new DoubleMatrix(nPoints);
+        DoubleMatrix bPerpDefo = new DoubleMatrix(nPoints);
 
-        for (int i = 0; i < LINENUMBER.length; ++i) {
-            Bperptopo[i] = topo_baseline.getBperp(LINENUMBER[i], PIXELNUMBER[i], 0);
-            Bperpdefo[i] = defo_baseline.getBperp(LINENUMBER[i], PIXELNUMBER[i], 0);
+        for (int i = 0; i < nPoints; ++i) {
+            bPerpTopo.put(i, topoBaseline.getBperp(position.get(i, 0), position.get(i, 1), 0));
+            bPerpDefo.put(i, defoBaseline.getBperp(position.get(i, 0), position.get(i, 1), 0));
         }
 
-        // ______ Now model ratio Bperpdefo/Bperptopo as linear ______
-        // ______ r(l,p) = a00 + a10*l + a01*p ______
-        // ______ give stats on max. error ______
+        // Now model ratio bPerpDefo/bPerpTopo as linear ______
+        //   ...r(l,p) = a00 + a10*l + a01*p ______
+        //   ...give stats on max. error ______
 
-        double[] Ratio = new double[Bperpdefo.length];
+        DoubleMatrix baselineRatio = bPerpDefo.div(bPerpTopo);
 
-        for (int l = 0; l < Bperpdefo.length; l++) {
-            Ratio[l] = Bperpdefo[l] / Bperptopo[l];
+        DoubleMatrix rhs = new DoubleMatrix(PolyUtils.polyFit2D(normalize(position.getColumn(0), minL, maxL),
+                normalize(position.getColumn(1), minP, maxP), baselineRatio, 1));
+
+//        /** per Tile: scale TOPO and subtract from DEFO : FOR TILE!!! */
+//        int numlines = (int) (tileWindow.lines() / masterMeta.getMlAz());
+//        int numpixels = (int) (tileWindow.pixels() / masterMeta.getMlRg());
+//        float firstline = (float) (tileWindow.linelo + (masterMeta.getMlAz() - 1.) / 2.);
+//        float firstpixel = (float) (tileWindow.pixlo + (masterMeta.getMlAz() - 1.) / 2.);
+
+        DoubleMatrix azimuthAxisNormalize = DoubleMatrix.linspace((int) tileWindow.linelo, (int) tileWindow.linehi, defoData.rows);
+        normalize_inplace(azimuthAxisNormalize, minL, maxL);
+
+        DoubleMatrix rangeAxisNormalize = DoubleMatrix.linspace((int) tileWindow.pixlo, (int) tileWindow.pixhi, defoData.columns);
+        normalize_inplace(rangeAxisNormalize, minP, maxP);
+
+        DoubleMatrix ratio = PolyUtils.polyval(azimuthAxisNormalize, rangeAxisNormalize, rhs, PolyUtils.degreeFromCoefficients(rhs.length));
+
+        DoubleMatrix scaledTopo = topoData.mul(ratio);
+        ComplexDoubleMatrix ratioBaselinesCplx = new ComplexDoubleMatrix(MatrixFunctions.cos(scaledTopo), MatrixFunctions.sin(scaledTopo).neg());
+
+        for (int i = 0; i < defoData.length; i++) {
+            if (defoData.data[i] == Double.NaN) {
+                defoData.data[i] = 0.0d;
+            }
         }
+        defoData.muli(ratioBaselinesCplx);
 
-        // ______ Set designmatrix, compute normalmatrix, righthandside ______
-        DoubleMatrix A = new DoubleMatrix(Ratio.length, 3);
-        for (int i = 0; i < A.rows; ++i) {
-            A.put(i, 0, 1.0);
-            A.put(i, 2, normalize2(PIXELNUMBER[i], minP, maxP));
-            A.put(i, 1, normalize2(LINENUMBER[i], minL, maxL));
-        }
-
-        DoubleMatrix N = LinearAlgebraUtils.matTxmat(A, A);
-        DoubleMatrix rhs = LinearAlgebraUtils.matTxmat(A, new DoubleMatrix(Ratio));
-
-        rhs = Solve.solve(N, rhs);
-
-
-//        // ______ Test inverse (thus stability cholesky) ______
-//        for (uint i = 0; i < Qx_hat.lines(); i++)
-//            for (uint j = 0; j < i; j++)
-//                Qx_hat(j, i) = Qx_hat(i, j);// repiar only stored Lower tri
-//        const real8 maxdev = max(abs(N * Qx_hat - eye(real8(Qx_hat.lines()))));
-//        INFO << "dinsar: max(abs(N*inv(N)-I)) = " << maxdev;
-//        INFO.print();
-//        if (maxdev > 0.01) {
-//            ERROR << ". Too large, normalization factors <-> crop?";
-//            PRINT_ERROR(ERROR.get_str())
-//            throw (some_error);
-//        } else if (maxdev > 0.001) {
-//            WARNING.print("Deviation quite large.  careful!");
-//        }
+////        DoubleMatrix ratioline = new DoubleMatrix(linspace((int) firstpixel, (int) (firstpixel + numpixels), (int) numpixels));
+//        DoubleMatrix ratioline = DoubleMatrix.linspace((int) firstpixel, (int) (firstpixel + numpixels - 1), numpixels);
+//        normalize_inplace(ratioline, minP, maxP);
+//        ratioline.muli(rhs[2]);  //     a01*p
+//        ratioline.addi(rhs[0]);  // a00+a01*p
 //
+//        // read in matrices line by line, correct phase ______
+////        ComplexDoubleMatrix DEFO = new ComplexDoubleMatrix(1, numpixels);    // buffer
 //
-//        // ______ Some other stuff for logfile ______
-//        //  matrix<real8> Qy_hat        = A * (matxmatT(Qx_hat,A));
-//        matrix<real8> y_hat = A * rhs;
-//        matrix<real8> e_hat = Ratio - y_hat;
+//        for (int i = 0; i < numlines - 1; ++i) {
 //
-//        uint pos, dummy;
-//        const real8 maxerrorratio = max(abs(e_hat), pos, dummy);
-//        const real8 maxrelerror = 100.0 * maxerrorratio / Ratio(pos, 0);
-//        INFO << "maximum error for l,p: " << LINENUMBER(pos, 0) << ","
-//                << PIXELNUMBER(pos, 0) << "; Ratio=" << Ratio(pos, 0)
-//                << " estimate=" << y_hat(pos, 0) << "; rel. err=" << maxrelerror << "%. ";
-//        INFO.print();
-//        if (maxrelerror < 5.0) {
-//            INFO.print("max err OK");
-//        } else {
-//            WARNING.print("max err quite large");
-//            WARNING.print("Error in deformation vector larger than 5% due to mismodeling baseline!");
-//        }
-
-
-        // ====== Per Tile
-        boolean writescaledtopo = false;
-
-        // scale TOPO and subtract from DEFO
-
-//        int numlines = (int) (defointerferogram.win.lines() / defointerferogram.multilookL);
-//        int numpixels = (int) (defointerferogram.win.pixels() / defointerferogram.multilookP);
-//        float firstline = (float) (defointerferogram.win.linelo + (defointerferogram.multilookL - 1.) / 2.);
-//        float firstpixel = (float) (defointerferogram.win.pixlo + (defointerferogram.multilookP - 1.) / 2.);
+//            // ratio = a00 + a10*LINE + a01*PIXEL
+//            double line = firstline + i * masterMeta.getMlAz();
+//            DoubleMatrix ratio = ratioline.add(rhs[1] * normalize2(line, minL, maxL));
 //
-//        DoubleMatrix ratioline = new DoubleMatrix(1, numpixels);
-//
-//        for (int i = 0; i < numpixels; ++i) {
-//            ratioline.put(0, i, firstpixel + i * defointerferogram.multilookP);
-//        }
-//
-//        normalize(ratioline, real4(minP), real4(maxP));
-//        ratioline *= real4(rhs(2, 0));         //     a01*p
-//        ratioline += real4(rhs(0, 0));         // a00+a01*p
-//
-//        // ______ read in matrices line by line, correct phase ______
-//        ComplexDoubleMatrix DEFO = new ComplexDoubleMatrix(1, numpixels);    // buffer
-//
-//        // test if reading phase was ok...
-//        //cerr << "test: writing hgt phase and cint.\n";
-//        //ofstream oftest1("TOPO.raw", ios::out | ios::binary | ios::trunc);
-//        //ofstream oftest2("CINT.raw", ios::out | ios::binary | ios::trunc);
-//
-//        for (int i = 0; i < numlines; ++i) {
-//
-//            // ______ ratio=a00+a10*l+a01*p ______
-//            double line = firstline + i * defointerferogram.multilookL;
-//            DoubleMatrix ratio = ratioline + real4(rhs(1, 0)) * normalize(line, real4(minL), real4(maxL));
-//
-//            // ______ read from file, correct, write to file ______
-//            ifdefocint >> DEFO;                         // read next full line
-//            const window filewin(i + 1, i + 1, 1, numpixels);
-//            matrix<real4> TOPO = topounwrappedinterf.readphase(filewin);
+//            // read from file, correct, write to file ______
+////            const window filewin(i + 1, i + 1, 1, numpixels);
+//            DoubleMatrix topoRow = topoData.getRow(i); //readphase(filewin);
+//            ComplexDoubleMatrix DEFO = defoData.getRow(i);
 //
 //            // seems faster, but how to check for unwrapping?
-//            //TOPO *= ratio;    // scaled topo to defo baseline
+//            //TOPO *= ratio;    // scaled topoData to defoData baseline
 //            //DEFO *= complr4(cos(TOPO),-sin(TOPO));
 //            // better matrix <int32> index = TOPO.find(NaN); later reset??
-//            // and topo=topo*ratio; and defo(index)=(0,0);
+//            // and topoData=topoData*ratio; and defoData(index)=(0,0);
 //            // but how to implement this best in matrixclass?
 //            // BK 24-Oct-2000
-//            for (int32 j = 0; j < numpixels; ++j) {
-//                (TOPO(0, j) == NaN) ?                // if unwrapping ok, then subtract scaled phase
-//                        DEFO(0, j) = complr4(0.0, 0.0) :
-//                        DEFO(0, j) *= complr4(fast_cos(ratio(0, j) * TOPO(0, j)), fast_min_sin(ratio(0, j) * TOPO(0, j)));
+//            for (int j = 0; j < numpixels - 1; ++j) {
+//                if (topoRow.get(0, j) == Double.NaN) {
+//                    DEFO.put(0, j, new ComplexDouble(0, 0));
+//                } else {
+//                    double realPart = ratio.get(j) * topoRow.get(j);
+//                    double imagPart = ratio.get(j) * topoRow.get(j);
+//                    DEFO.put(j, DEFO.get(j).mul(new ComplexDouble(Math.cos(realPart), Math.sin(imagPart))));
+////                    DEFO.put(0, j, DEFO.get(0, j).mul(new ComplexDouble(Math.cos(realPart), Math.sin(imagPart))));
+//                }
+////                (TOPO(0, j) == NaN) ?                // if unwrapping ok, then subtract scaled phase
+////                        DEFO(0, j) = complr4(0.0, 0.0) :
+////                        DEFO(0, j) *= complr4(fast_cos(ratio(0, j) * TOPO(0, j)), fast_min_sin(ratio(0, j) * TOPO(0, j)));
 //            }
-//            ofcint << DEFO;             // now topo-corrected phase
+//
+//            tempData.putRow(i, DEFO);
 //
 //        }
 
     }
+
+    public static double[] linspace(final int lower, final int upper, final int size) {
+        double[] result = new double[size];
+        for (int i = 0; i < size; i++) {
+            double t = (double) i / (size - 1);
+            result[i] = lower * (1 - t) + t * upper;
+        }
+        return result;
+    }
+
+    public static double[] linspace(final int lower, final int upper) {
+        return linspace(lower, upper, 100);
+    }
+
+    private void normalize_inplace(DoubleMatrix data, double min, double max) {
+        data.subi(.5 * (min + max));
+        data.divi(.25 * (max - min));
+    }
+
+    private DoubleMatrix normalize(DoubleMatrix in, double min, double max) {
+        DoubleMatrix out = in.dup();
+        out.subi(.5 * (min + max));
+        out.divi(.25 * (max - min));
+        return out;
+    }
+
 }
