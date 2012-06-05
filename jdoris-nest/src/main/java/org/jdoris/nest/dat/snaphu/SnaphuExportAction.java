03a214ff5f95f42213331d41dcb9853cb9bc80d2
Petar Marinkovic <petarmar@gmail.com>
2012-06-05 14:25:29 +0200

    Snaphu data Export/Import operators: clean-up, profiling and polishing (lots of small fixes)

--- jdoris-nest/src/main/java/org/jdoris/nest/dat/snaphu/SnaphuExportAction.java
+++ jdoris-nest/src/main/java/org/jdoris/nest/dat/snaphu/SnaphuExportAction.java
@@ -1,4 +1,4 @@
-package org.jdoris.nest.dat.unwrapping;
+package org.jdoris.nest.dat.snaphu;
 
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.visat.VisatApp;
@@ -7,15 +7,15 @@ import org.esa.nest.dat.plugins.graphbuilder.GraphBuilderDialog;
 
 import java.io.File;
 
-public class SnaphuImportAction extends AbstractVisatAction {
+public class SnaphuExportAction extends AbstractVisatAction {
     @Override
     public void actionPerformed(CommandEvent event) {
         final GraphBuilderDialog dialog = new GraphBuilderDialog(VisatApp.getApp(),
-                "Snaphu Data Import", "SnaphuImportOp", false);
+                "Snaphu Data Export", "SnaphuExportOp", false);
         dialog.show();
 
         final File graphPath = GraphBuilderDialog.getInternalGraphFolder();
-        final File graphFile = new File(graphPath, "SnaphuImportGraph.xml");
+        final File graphFile = new File(graphPath, "SnaphuExportGraph.xml");
 
         dialog.LoadGraph(graphFile);
 
--- jdoris-nest/src/main/java/org/jdoris/nest/dat/snaphu/SnaphuImportAction.java
+++ jdoris-nest/src/main/java/org/jdoris/nest/dat/snaphu/SnaphuImportAction.java
@@ -1,4 +1,4 @@
-package org.jdoris.nest.dat.unwrapping;
+package org.jdoris.nest.dat.snaphu;
 
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.visat.VisatApp;
@@ -7,15 +7,15 @@ import org.esa.nest.dat.plugins.graphbuilder.GraphBuilderDialog;
 
 import java.io.File;
 
-public class SnaphuExportAction extends AbstractVisatAction {
+public class SnaphuImportAction extends AbstractVisatAction {
     @Override
     public void actionPerformed(CommandEvent event) {
         final GraphBuilderDialog dialog = new GraphBuilderDialog(VisatApp.getApp(),
-                "Snaphu Data Export", "SnaphuExportOp", false);
+                "Snaphu Data Import", "SnaphuImportOp", false);
         dialog.show();
 
         final File graphPath = GraphBuilderDialog.getInternalGraphFolder();
-        final File graphFile = new File(graphPath, "SnaphuExportGraph.xml");
+        final File graphFile = new File(graphPath, "SnaphuImportGraph.xml");
 
         dialog.LoadGraph(graphFile);
 
